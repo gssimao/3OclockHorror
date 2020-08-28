49a552d2abcdee4077ab3f86b24dcd0d6bd3ed86
Ryan M. (Empathy) <ryan.marnette@yahoo.com>
2020-08-28 10:13:11 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ 3OclockHorror/Assets/Scene Tester/Ryan/Scripts/Inventory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<Item> characterItems = new List<Item>();
+    public ItemDatabase itemDatabase;
+
+    private void Start()
+    {
+        GiveItem("Test Object");
+    }
+
+    public void GiveItem(int id)
+    {
+        Item itemToAdd = itemDatabase.GetItem(id);
+        characterItems.Add(itemToAdd);
+        Debug.Log("Added item: " + itemToAdd.title);
+    }
+
+    public void GiveItem(string itemName)
+    {
+        Item itemToAdd = itemDatabase.GetItem(itemName);
+        characterItems.Add(itemToAdd);
+        Debug.Log("Added Item: " + itemToAdd.title);
+    }
+}
