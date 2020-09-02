0beb37e56773dc4324e21066997beb9eadf67801
dvminder <dean.minder@trojans.dsu.edu>
2020-09-02 10:20:50 -0500

    finished prototype pillar puzzle

--- 3OclockHorror/Assets/Scene Tester/Dean/DeanScripts/test.cs
+++ 3OclockHorror/Assets/Scene Tester/Dean/DeanScripts/test.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
+    private bool pillar1 = false;
+    private bool pillar2 = false;
+    private bool pillar3 = false;
+    private bool pillar4 = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,22 +23,50 @@ public class test : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("trigger");
         if(other.gameObject.tag == "Pillar3")
         {
             Debug.Log("Pillar3 good");
+            pillar3 = true;
         }
         if(other.gameObject.tag == "Pillar1")
         {
             Debug.Log("Pillar1 good");
+            pillar1 = true;
         }
         if(other.gameObject.tag == "Pillar2")
         {
             Debug.Log("Pillar2 good");
+            pillar2 = true;
         }
         if(other.gameObject.tag == "Pillar4")
         {
             Debug.Log("Pillar4 good");
-        }    
+            pillar4 = true;
+        }
+
+        if(pillar1 == true && pillar2 == true && pillar3 == true && pillar4 == true)
+        {
+            Debug.Log("Puzzle solved");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Pillar3")
+        {
+            pillar3 = true;
+        }
+        if (other.gameObject.tag == "Pillar1")
+        {
+            pillar1 = true;
+        }
+        if (other.gameObject.tag == "Pillar2")
+        {
+            pillar2 = true;
+        }
+        if (other.gameObject.tag == "Pillar4")
+        {
+            pillar4 = true;
+        }
     }
 }
