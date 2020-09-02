70655c43d976eefd47c89ebb51f5736827dc4089
Benjamin <benjamin.henschen@gmail.com>
2020-09-02 11:19:55 -0500

    v0.019
    Added an End Scene for a game over

---
+++ 3OclockHorror/Assets/Scene Tester/Ben/Scripts/EndScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
+{
+    public string gameScene;
+    public string menuScene;
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(menuScene);
+    }
+}
