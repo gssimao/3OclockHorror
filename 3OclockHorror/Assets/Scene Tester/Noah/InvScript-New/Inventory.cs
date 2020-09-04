310a97ce5b19e2538f898ddc72d2f15ba801fe8a
Noah <noah.osborn@trojans.dsu.edu>
2020-09-04 12:45:13 -0500

    v0.021.2
    Began testing drag and drop inventory. Isn't working yet.

---
+++ 3OclockHorror/Assets/Scene Tester/Noah/InvScript-New/Inventory.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public static Inventory instance;
+    public List<Item> items; //List of items - size is managed by editor. This allows dynamic per instance sizes without complicated stuff.
+    public inventorySlot[] slots; //List of slots
+    public Transform itemsParent;
+
+    public delegate void onItemChanged();
+    public onItemChanged onItemChangedCallback;
+
+    void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("More than one Inventory");
+        }
+        else
+        {
+            instance = this;
+        }
+
+        slots = itemsParent.GetComponentsInChildren<inventorySlot>();
+
+        if(slots.Length != items.Count)
+        {
+            Debug.LogError("Slots / Items count differs");
+        }
+    }
+
+    
+    public void AddItem(Item itemToAdd)
+    {
+        if(items[0] == null)
+        {
+            items[0] = itemToAdd;
+        }
+    }
+    public void RemoveItem(Item itemToRemove)
+    {
+        if (items[0] != null)
+        {
+            items[0] = null;
+        }
+    }   
+}
---
+++ 3OclockHorror/Assets/Scene Tester/Noah/InvScript-New/inventorySlot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class inventorySlot : MonoBehaviour
+{
+    public Image icon;
+    Item item;
+
+    public void addItem(Item itemToAdd)
+    {
+        item = itemToAdd;
+
+        icon.sprite = item.sprite;
+        icon.enabled = true;
+    }
+
+    public void removeItem()
+    {
+        item = null;
+
+        icon.sprite = null;
+        icon.enabled = false;
+    }
+}
