ee84b24c24fcc36e15bb95a9514082220d207e98
gabe <gabriel.simao@trojans.dsu.edu>
2020-09-07 00:42:30 -0500

    v0.022
    Adding LeanTween and the pocket puzzle

--- 3OclockHorror/Assets/Scene Tester/Gabe/Script/RotationMedGear.cs
+++ 3OclockHorror/Assets/Scene Tester/Gabe/Script/RotationMedGear.cs
@@ -7,30 +7,115 @@ public class RotationMedGear : MonoBehaviour
     public GameObject BigGear;
     public GameObject smallGear;
 
-    Vector3 mPrevPos = Vector3.zero;
-    Vector3 mPosDelta = Vector3.zero;
+    int[] bigGearPosition = new int[] { 2, 25, 53, 81, 110, 138, 165, 193, 220, 248, 275, 305, 332 };
+    int[] medGearPosition = new int[] { 0, 60, 120, 180, 240, 300 };
+    int[] smallGearPosition = new int[] { 0, 120, 240 }; // all z rotation location that this should turn to
+    public int Medmovement = 0;
 
-    private bool moveMedGear = false;
+    //BigGear.GetComponent<RotationBigGear>().Bigmovement
 
     private void OnMouseOver()
     {
-        moveMedGear = true;
-    }
-    private void OnMouseExit()
-    {
-        moveMedGear = false;
+        if (Input.GetMouseButtonUp(1)) //this should turn the big to the right
+        {
+            BigGear.GetComponent<RotationBigGear>().Bigmovement = ControlBound(BigGear.GetComponent<RotationBigGear>().Bigmovement, 13, false);
+            Medmovement = ControlBound(Medmovement, 6, true);
+            smallGear.GetComponent<RotationSmallGear>().Smallmovement = ControlBound(smallGear.GetComponent<RotationSmallGear>().Smallmovement, 3, true);
+
+            LeanTween.rotateZ(BigGear, bigGearPosition[BigGear.GetComponent<RotationBigGear>().Bigmovement], 1); // move big gear
+            LeanTween.rotateZ(gameObject, medGearPosition[Medmovement], 1); // move med gear
+            LeanTween.rotateZ(smallGear, smallGearPosition[smallGear.GetComponent<RotationSmallGear>().Smallmovement], 1); // move small gear
+        }
+        if (Input.GetMouseButtonUp(0)) //this should turn the big to the left
+        {
+            BigGear.GetComponent<RotationBigGear>().Bigmovement = ControlBound(BigGear.GetComponent<RotationBigGear>().Bigmovement, 13, true);
+            Medmovement = ControlBound(Medmovement, 6, false);
+            smallGear.GetComponent<RotationSmallGear>().Smallmovement = ControlBound(smallGear.GetComponent<RotationSmallGear>().Smallmovement, 3, false);
+
+            LeanTween.rotateZ(BigGear, bigGearPosition[BigGear.GetComponent<RotationBigGear>().Bigmovement], 1);
+            LeanTween.rotateZ(gameObject, medGearPosition[Medmovement], 1); // move med gear
+            LeanTween.rotateZ(smallGear, smallGearPosition[smallGear.GetComponent<RotationSmallGear>().Smallmovement], 1); // move small gear
+        }
     }
 
-    void Update()
+    private int ControlBound(int moviment, int size, bool addOrSubtract) // the bool should tell if the we are adding or subtracting. True is adding
     {
-        if (Input.GetMouseButton(0) && moveMedGear == true)
+        switch (size)
         {
-            Debug.Log("this is med gear");
-            mPosDelta = Input.mousePosition - mPrevPos;
-            this.transform.Rotate(transform.forward, -2 * Vector3.Dot(mPosDelta, Camera.main.transform.right), Space.Self); // this is med gear
-            BigGear.transform.Rotate(transform.forward, Vector3.Dot(mPosDelta, Camera.main.transform.right), Space.Self);
-            smallGear.transform.Rotate(transform.forward, -3 * Vector3.Dot(mPosDelta, Camera.main.transform.right), Space.Self);
+            case 13:
+                for (int i = 0; i < 2; i++)
+                { 
+                    if (addOrSubtract == true)
+                    {
+                        moviment++;
+                    }
+                    if (addOrSubtract == false)
+                    {
+                        moviment--;
+                    }
+                    if (moviment > 12)
+                    {
+                        moviment = 0;
+                    }
+                    if (moviment < 0)
+                    {
+                        moviment = 12;
+                    }
+                }
+                break;
+            case 6:
+               
+               if (addOrSubtract == true)
+               {
+                  moviment++;
+               }
+               if (addOrSubtract == false)
+               {
+                   moviment--;
+               }
+               if (moviment > 5)
+               {
+                   moviment = 0;
+               }
+                if (moviment < 0)
+                {
+                    moviment = 5;
+                }
+
+                break;
+            case 3:
+                for (int i = 0; i < 5; i++)
+                {
+                    if (addOrSubtract == true)
+                    {
+                        moviment++;
+                    }
+                    if (addOrSubtract == false)
+                    {
+                        moviment--;
+                    }
+
+                    if (moviment > 2)
+                    {
+                        moviment = 0;
+                    }
+                    if (moviment < 0)
+                    {
+                        moviment = 2;
+                    }
+                }
+                break;
+            default:
+                Debug.Log("The size is incorrect put either 3, 6, or 13 for size");
+                break;
         }
-        mPrevPos = Input.mousePosition;
+        return moviment;
     }
 }
+
+/*
+b m  s           b  m  s
+b 1  3  5        b  R  L  R
+m 1  2  3        m  L  R  L
+s  1  2  1      s   R  L  R
+*/
--- 3OclockHorror/Assets/Scene Tester/Gabe/Script/RotationSmallGear.cs
+++ 3OclockHorror/Assets/Scene Tester/Gabe/Script/RotationSmallGear.cs
@@ -7,30 +7,111 @@ public class RotationSmallGear : MonoBehaviour
     public GameObject BigGear;
     public GameObject MedGear;
 
-    Vector3 mPrevPos = Vector3.zero;
-    Vector3 mPosDelta = Vector3.zero;
+    int[] bigGearPosition = new int[] { 2, 25, 53, 81, 110, 138, 165, 193, 220, 248, 275, 305, 332 };
+    int[] medGearPosition = new int[] { 0, 60, 120, 180, 240, 300 };
+    int[] smallGearPosition = new int[] { 0, 120, 240 }; // all z rotation location that this should turn to
 
-    private bool moveSmallGear = false;
+    public int Smallmovement = 0;
+
+
+
     private void OnMouseOver()
     {
-        moveSmallGear = true;
-    }
-    private void OnMouseExit()
-    {
-        moveSmallGear = false;
-    }
-
-
-    void Update()
-    {
-        if (Input.GetMouseButton(0) && moveSmallGear == true)
+        if (Input.GetMouseButtonUp(1)) //this should turn the big to the right
         {
-            Debug.Log("this is small gear");
-            mPosDelta = Input.mousePosition - mPrevPos;
-            this.transform.Rotate(transform.forward, -Vector3.Dot(mPosDelta, Camera.main.transform.right), Space.Self);
-            BigGear.transform.Rotate(transform.forward, Vector3.Dot(mPosDelta, Camera.main.transform.right), Space.Self);
-            MedGear.transform.Rotate(transform.forward, 2 * Vector3.Dot(mPosDelta, Camera.main.transform.right), Space.Self);
+            BigGear.GetComponent<RotationBigGear>().Bigmovement = ControlBound(BigGear.GetComponent<RotationBigGear>().Bigmovement, 13, true);
+            MedGear.GetComponent<RotationMedGear>().Medmovement = ControlBound(MedGear.GetComponent<RotationMedGear>().Medmovement, 6, false);
+            Smallmovement = ControlBound(Smallmovement, 3, true);
+
+            LeanTween.rotateZ(BigGear, bigGearPosition[BigGear.GetComponent<RotationBigGear>().Bigmovement], 1); // move big gear
+            LeanTween.rotateZ(MedGear, medGearPosition[MedGear.GetComponent<RotationMedGear>().Medmovement], 1); // move med gear
+            LeanTween.rotateZ(gameObject, smallGearPosition[Smallmovement], 1); // move small gear
         }
-        mPrevPos = Input.mousePosition;
+        if (Input.GetMouseButtonUp(0)) //this should turn the big to the left
+        {
+            BigGear.GetComponent<RotationBigGear>().Bigmovement = ControlBound(BigGear.GetComponent<RotationBigGear>().Bigmovement, 13, false);
+            MedGear.GetComponent<RotationMedGear>().Medmovement = ControlBound(MedGear.GetComponent<RotationMedGear>().Medmovement, 6, true);
+            Smallmovement = ControlBound(Smallmovement, 3, false);
+
+            LeanTween.rotateZ(BigGear, bigGearPosition[BigGear.GetComponent<RotationBigGear>().Bigmovement], 1);
+            LeanTween.rotateZ(MedGear, medGearPosition[MedGear.GetComponent<RotationMedGear>().Medmovement], 1); // move med gear
+            LeanTween.rotateZ(gameObject, smallGearPosition[Smallmovement], 1); // move small gear
+        }
+    }
+
+
+    private int ControlBound(int moviment, int size, bool addOrSubtract) // the bool should tell if the we are adding or subtracting. True is adding
+    {
+        switch (size)
+        {
+            case 13:
+                if (addOrSubtract == true)
+                {
+                    moviment++;
+                }
+                if (addOrSubtract == false)
+                {
+                    moviment--;
+                }
+                if (moviment > 12)
+                {
+                    moviment = 0;
+                }
+                if (moviment < 0)
+                {
+                    moviment = 12;
+                }
+                break;
+            case 6:
+                for (int i = 0; i < 2; i++)
+                {
+                    if (addOrSubtract == true)
+                    {
+                        moviment++;
+                    }
+                    if (addOrSubtract == false)
+                    {
+                        moviment--;
+                    }
+                    if (moviment > 5)
+                    {
+                        moviment = 0;
+                    }
+                    if (moviment < 0)
+                    {
+                        moviment = 5;
+                    }
+                }
+                break;
+            case 3:
+               if (addOrSubtract == true)
+               {
+                   moviment++;
+               }
+               if (addOrSubtract == false)
+               {
+                   moviment--;
+               }
+               if (moviment > 2)
+               {
+                   moviment = 0;
+               }
+               if (moviment < 0)
+               {
+                   moviment = 2;
+               }
+                break;
+            default:
+                Debug.Log("The size is incorrect put either 3, 6, or 13 for size");
+                break;
+        }
+        return moviment;
     }
 }
+
+/*
+b m  s           b  m  s
+b 1  3  5        b  R  L  R
+m 1  2  3        m  L  R  L
+s  1  2  1      s   R  L  R
+*/
