a6bc4a52397af67a8617505412d425d1477cc742
Noah <noah.osborn@trojans.dsu.edu>
2020-09-10 15:20:43 -0500

    v0.027.2
    Created workbench slots to prepare for puzzle states.

---
+++ 3OclockHorror/Assets/Alpha Test/Scripts/InvScript-New/invUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class invUI : MonoBehaviour
+{
+    public Canvas myUI;
+    public Camera invCamera;
+    public Camera Main;
+    public Canvas timeUI;
+    bool active;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myUI.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("r"))
+        {
+            if(!active)
+            {
+                activateUI();
+            }
+            else
+            {
+                deactivateUI();
+            }
+        }
+    }
+
+    public void activateUI()
+    {
+        Main.gameObject.SetActive(false);
+        timeUI.gameObject.SetActive(false);
+        invCamera.gameObject.SetActive(true);
+        myUI.gameObject.SetActive(true);
+
+        active = true;
+    }
+    public void deactivateUI()
+    {
+        Main.gameObject.SetActive(true);
+        timeUI.gameObject.SetActive(true);
+        invCamera.gameObject.SetActive(false);
+        myUI.gameObject.SetActive(false);
+
+        active = false;
+    }
+}
--- 3OclockHorror/Assets/Alpha Test/Scripts/workbench_cntrl.cs
+++ 3OclockHorror/Assets/Alpha Test/Scripts/workbench_cntrl.cs
@@ -7,25 +7,42 @@ public class workbench_cntrl : MonoBehaviour
     public GameObject player;
     public GameObject timeUI; //Time UI, used to deactivate for screenclutter.
     public GameObject myWorkspace; //The canvas for this specific workbench to work with.
-    public GameObject ePrompt; //Prompt to press E - Will likely depreciate beyond alpha
+
+    invUI invUI;
+    bool active;
+
+    //public GameObject ePrompt; //Prompt to press E - Will likely depreciate beyond alpha
+    private void Start()
+    {
+        invUI = player.GetComponent<invUI>();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
         float dist = Vector3.Distance(player.transform.position, transform.position); //Get the position of player
         if(dist <= 1.5f) //If the player is in range
-        { 
-            ePrompt.SetActive(true); //Show the prompt
-
-            if (Input.GetKeyDown("e")) //if E is pressed
-            {
-                timeUI.SetActive(false); //Change the active UI around
-                myWorkspace.SetActive(true);
-            }
-        }
-        else
         {
-            ePrompt.SetActive(false); //If out of range, deactivate the prompt.
+            if (Input.GetKeyDown("e"))
+            {
+                if (!active) 
+                {
+                    invUI.activateUI();
+                    myWorkspace.SetActive(true);
+
+                    timeUI.SetActive(false);
+                    active = true;
+                }
+                else
+                {
+                    myWorkspace.SetActive(false);
+                    invUI.deactivateUI();
+
+                    timeUI.SetActive(true);
+                    active = false;
+                }
+            }
         }
     }
 }
