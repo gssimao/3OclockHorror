d60da5f688745c166215af0b4eea180dff1c166a
Noah <noah.osborn@trojans.dsu.edu>
2020-09-15 14:39:50 -0500

    v0.030.4
    First idle state implemented. Next is to begin playing around with navmesh for navigation

---
+++ 3OclockHorror/Assets/Scene Tester/Noah/Scripts/FSM/States/idleState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="IdleState", menuName = "FSM/States/Idle", order = 1)]
+public class idleState : abstractFSMState
+{
+    public override bool enterState()
+    {
+        base.enterState();
+        Debug.Log("Entered Idle state");
+        return true;
+    }
+
+    public override void updateState()
+    {
+        Debug.Log("Updating idle state");
+    }
+
+    public override bool exitState()
+    {
+        base.exitState();
+        Debug.Log("Exiting idle state");
+        return true;
+    }
+
+
+}
