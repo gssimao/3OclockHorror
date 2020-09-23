4a94bc6530143145db4992bd5e220f42ec1c3ccc
Benjamin <benjamin.henschen@gmail.com>
2020-09-23 10:56:14 -0500

    V1.001.2
    Began Updating the Jounral script and UI

--- 3OclockHorror/Assets/Scene Tester/Ben/Scripts/SanityManager.cs
+++ 3OclockHorror/Assets/Scene Tester/Ben/Scripts/SanityManager.cs
@@ -6,6 +6,14 @@ public class SanityManager : MonoBehaviour
 {
     public float sanityValue; //Variable that holds how much sanity the player has
 
+    private void Update()
+    {
+        if(sanityValue <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
     public void ChangeSanity(float changeValue)
     {
         sanityValue = sanityValue + changeValue;
---
+++ 3OclockHorror/Assets/Scene Tester/Ben/Scripts/SwitchPage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchPage : MonoBehaviour
+{
+    public RectTransform[] Pages = new RectTransform[3];
+
+    RectTransform[] Buttons = new RectTransform[3];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Buttons[0] = Pages[0].GetComponentInChildren<RectTransform>();
+        Buttons[1] = Pages[1].GetComponentInChildren<RectTransform>();
+        Buttons[2] = Pages[2].GetComponentInChildren<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangePage(int page)
+    {
+        if (page == 0)
+        {
+            
+        }
+        else if (page == 1)
+        {
+
+        }
+        else if (page == 2)
+        {
+            Buttons[page].anchoredPosition3D = new Vector3(Mathf.Abs(Buttons[page].anchoredPosition3D.x), Buttons[page].anchoredPosition3D.y, Buttons[page].anchoredPosition3D.x);
+            Pages[page].anchoredPosition3D = new Vector3(Pages[page].anchoredPosition3D.x, Pages[page].anchoredPosition3D.y, -35.0f);
+        }
+    }
+}
