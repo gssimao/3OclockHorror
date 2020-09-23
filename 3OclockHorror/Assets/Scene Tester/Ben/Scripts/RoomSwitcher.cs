7540aa6e61d4def9fa71fa55743292e3ed9010b3
Benjamin <benjamin.henschen@gmail.com>
2020-09-22 19:21:45 -0500

    V1.0
    First Build

---
+++ 3OclockHorror/Assets/Scene Tester/Ben/Scripts/RoomSwitcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSwitcher : MonoBehaviour
+{
+    public room firstRoom;
+    public room secondRoom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerMovement>().myRoom == firstRoom)
+        {
+            other.GetComponent<PlayerMovement>().myRoom = secondRoom;
+        }
+        else if (other.GetComponent<PlayerMovement>().myRoom == secondRoom)
+        {
+            other.GetComponent<PlayerMovement>().myRoom = firstRoom;
+        }
+    }
+}
