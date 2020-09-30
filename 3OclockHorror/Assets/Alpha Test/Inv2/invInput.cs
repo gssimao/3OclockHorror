2484179b0fdc955a3257dc3af5610907fe0fadc2
Noah <noah.osborn@trojans.dsu.edu>
2020-09-30 10:40:12 -0500

    v1.005.4
    Implementing new inventory system

---
+++ 3OclockHorror/Assets/Alpha Test/Inv2/invInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class invInput : MonoBehaviour
+{
+
+    [SerializeField]
+    KeyCode invKey;
+    [SerializeField]
+    GameObject invCanvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(invKey))
+        {
+            if (invCanvas.activeSelf)
+            {
+                invCanvas.SetActive(false);
+            }
+            else
+            {
+                invCanvas.SetActive(true);
+            }
+        }
+    }
+
+    /*
+    public void ShowCursor()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void HideCursor()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    */
+}
