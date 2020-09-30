f32c6938aabb06dc67308100445d61cbd55a5ec0
Noah <noah.osborn@trojans.dsu.edu>
2020-09-30 09:13:54 -0500

    InvRework 1.0
    Finished

--- 3OclockHorror/Assets/Scene Tester/Noah/Inv2/ContainerControl.cs
+++ 3OclockHorror/Assets/Scene Tester/Noah/Inv2/ContainerControl.cs
@@ -14,25 +14,12 @@ public class ContainerControl : MonoBehaviour
     InventoryManager IM;
     bool active; //Am I the active workbench inventory?
 
-    Item[] myItems;
-
     // Start is called before the first frame update
     void Start()
     {
-        if(cntnrDisp == null)
-        {
-            Debug.LogError("hey wtf wheres the container display");
-        }
         if (myInv == null)
         {
             myInv = gameObject.GetComponent<Inventory>();
-        }
-        if (myItems != null)
-        {
-            foreach (Item item in myItems)
-            {
-                myInv.AddStartingItem(item);
-            }
         }
         active = false;
         myInv.CloseInv();
@@ -47,7 +34,7 @@ public class ContainerControl : MonoBehaviour
             if (Input.GetKeyDown("e") && !active)
             {
                 IM.ActivateInventory(myInv);
-                myInv.OpenInv(); //Update the items to be in accordance with the items array
+                myInv.OpenInv();
                 active = true;
                 cntnrDisp.SetActive(true);
             }
