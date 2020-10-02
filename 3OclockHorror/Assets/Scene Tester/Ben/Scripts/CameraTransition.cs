ac8bf9c04ee7a11d538710e22701904c84af4c1b
Benjamin <benjamin.henschen@gmail.com>
2020-10-02 10:58:04 -0500

    v1.005.11
    Worked on Transitions should be finished soon

---
+++ 3OclockHorror/Assets/Scene Tester/Ben/Scripts/CameraTransition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class CameraTransition : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    public void CameraCrossfade()
+    {
+        StartCoroutine(ChangeCamera());
+    }
+
+    IEnumerator ChangeCamera()
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+    }
+}
