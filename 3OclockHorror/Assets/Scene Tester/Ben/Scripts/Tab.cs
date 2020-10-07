35548a9790dfcefdd39ae340d7a6d1481e96be58
Benjamin <benjamin.henschen@gmail.com>
2020-10-07 10:59:42 -0500

    v1.007.4
    Added door sprites and library to main scene, waiting on garage.

---
+++ 3OclockHorror/Assets/Scene Tester/Ben/Scripts/Tab.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tab : MonoBehaviour
+{
+    [SerializeField]
+    GameObject itemSlotsGrid;
+
+    ItemSlot[] itemSlots;
+
+    Item invItems;
+
+    #region Get/Set
+
+    public Item getItems()
+    {
+        itemSlots = itemSlotsGrid.GetComponentsInChildren<ItemSlot>();
+        for (int i = 0; i < itemSlots.Length; i++)
+        {
+            invItems = itemSlots[].Item;
+        }
+
+        return invItems;
+    }
+
+    #endregion
+}
