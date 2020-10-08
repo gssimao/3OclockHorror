e48c6d5042349135d013378d903df20388453d4e
Ryan M <ryan.marnette@gmail.com>
2020-10-08 16:14:57 -0500

    v1.009.3
    Added Tooltip script and workbench collider

---
+++ 3OclockHorror/Assets/Scene Tester/Ryan/Scripts/Tooltip.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tooltip : MonoBehaviour
+{
+    public Text TooltipText;
+    private bool walked = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TooltipText = GameObject.Find("TooltipText").GetComponent<Text>();
+        TooltipText.text = "Use 'WASD' to move";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameObject.Find("Player2").GetComponent<PlayerMovement>().walking == false && walked == false)
+        {
+            TooltipText.text = "";
+            walked = true;
+        }
+    }
+}
