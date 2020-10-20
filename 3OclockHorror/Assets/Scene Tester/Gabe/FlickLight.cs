9fd454696f2542fc48210997c1fafac9c08ab944
gabe <gabriel.simao@trojans.dsu.edu>
2020-10-19 22:45:18 -0500

    v2.010
    adding animation for the pendulum and script for the  candles

---
+++ 3OclockHorror/Assets/Scene Tester/Gabe/FlickLight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickLight : MonoBehaviour
+{
+    public SpriteMask spriteMask;
+    public float flick;
+    public float amplitude = 0.3f;
+    public float omega = 1.0f;
+    float index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteMask = GetComponent<SpriteMask>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        index += Time.deltaTime;
+        //flick = Random.Range(0f, 0.2f);
+
+        flick = Mathf.Abs(amplitude * Mathf.Sin(omega*index));
+        spriteMask.alphaCutoff = flick;
+    }
+}
