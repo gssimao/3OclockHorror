82c40b5a9b4101596564bdc30115efcce0db69bd
gabe <gabriel.simao@trojans.dsu.edu>
2020-11-02 00:41:37 -0600

    v2.018
    first title screen art is in

--- 3OclockHorror/Assets/Scene Tester/Ben/Scripts/MainMenu.cs
+++ 3OclockHorror/Assets/Scene Tester/Ben/Scripts/MainMenu.cs
@@ -7,13 +7,26 @@ public class MainMenu : MonoBehaviour
 {
     public string mMenuScene;
     public AudioManager menuAudio;
+    public Material material;
+
+    public float amplitude = 1.4f;
+    public float omega = .3f;
+    float index;
+    float wave;
+
 
     void Awake()
     {
         menuAudio.Play("Theme");
     }
+    private void Update()
+    {
+        index += Time.deltaTime;
+        wave = Mathf.Abs(amplitude * Mathf.Sin(omega * index));
+        material.SetFloat("_LightWave", wave);
+    }
 
-   public void PlayGame()
+    public void PlayGame()
     {
         menuAudio.Stop("Theme");
         SceneManager.LoadScene(mMenuScene);
