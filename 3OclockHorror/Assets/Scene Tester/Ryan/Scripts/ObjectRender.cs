c9920746c7d056c56ed168572efa8bd7fe80f7e1
Ryan M <ryan.marnette@gmail.com>
2020-11-18 02:18:09 -0600

    v3.006.6
    Added an ObjectRender script to allow the user to see larger versions of the sprite.

---
+++ 3OclockHorror/Assets/Scene Tester/Ryan/Scripts/ObjectRender.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectRender : MonoBehaviour
+{
+    private GameObject item;
+    public Sprite sprite;
+    private Image image;
+    private Sprite transparent;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        image.sprite = sprite;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        image.sprite = transparent;
+    }
+
+    void Start()
+    {
+        item = GameObject.Find("Image");
+        image = GameObject.Find("PopupImage").GetComponent<Image>();
+        transparent = image.sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
