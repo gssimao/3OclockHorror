e669f1d29359c5d6920da35be7e1f7f19d4b93df
Benjamin <benjamin.henschen@gmail.com>
2021-01-12 17:10:01 -0600

    v3.019
    Floors 2 and 3 have been added to main hub

---
+++ 3OclockHorror/Assets/Scene Tester/Ben/Scripts/FloorChanger.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FloorChanger : MonoBehaviour
+{
+    public string sceneName; //name of the scene to transfer too
+    Scene currentScene;
+    public GameObject spawnPoint;
+    public GameObject player;
+    public invInput Listener;
+    public room destRoom;
+
+    public Animator transition;
+    public Animator blackWallanim;
+    public GameObject crossFade;
+    public GameObject blackWall;
+
+    float dist;
+    bool animIsDone = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(player.transform.position, this.transform.position);
+        if (Mathf.Abs(dist) <= 0.6f)
+        {
+            Listener.enabled = false;
+            if (Input.GetKeyDown("e"))
+            {
+                blackWall.SetActive(true);
+                crossFade.SetActive(true);
+                transition.SetTrigger("End");
+                animIsDone = true;
+                player.transform.position = spawnPoint.transform.position;
+                player.GetComponent<PlayerMovement>().myRoom = destRoom;
+            }
+            /*if (blackWallanim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.85 && animIsDone)
+            {
+                player.GetComponent<PlayerMovement>().tempCanvases.Clear();
+                animIsDone = false;
+                StartCoroutine(LoadYourAsyncScene(player));
+            }*/
+        }
+        else
+        {
+            if (Listener != null)
+            {
+                Listener.enabled = true;
+            }
+        }
+    }
+
+    IEnumerator LoadYourAsyncScene(GameObject Instance)
+    {
+        currentScene = SceneManager.GetActiveScene();
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        player.transform.position = spawnPoint.transform.position;
+        blackWall.SetActive(false);
+        crossFade.SetActive(false);
+
+        SceneManager.MoveGameObjectToScene(Instance, SceneManager.GetSceneByName(sceneName));
+
+        SceneManager.UnloadSceneAsync(currentScene);
+    }
+}
--- 3OclockHorror/Assets/Scene Tester/Ben/Scripts/sceneManager.cs
+++ 3OclockHorror/Assets/Scene Tester/Ben/Scripts/sceneManager.cs
@@ -8,7 +8,7 @@ public class sceneManager : MonoBehaviour
     public PlayerMovement player;
     public invInput Listener;
     public GameObject invCanv;
-    public SceneChanger sceneChanger;
+    public FloorChanger sceneChanger;
 
     //Gameobject Variables
     public roomCntrl[] emptyObjectsRC;
