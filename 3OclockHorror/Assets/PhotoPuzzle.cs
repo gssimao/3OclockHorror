79fe01e760e888a719cc44fc8725d5cfaa1e6cdc
Benjamin <benjamin.henschen@gmail.com>
2021-01-28 18:20:15 -0600

    v3.025.5
    Half of photo puzzle behavior has been implemented

---
+++ 3OclockHorror/Assets/PhotoPuzzle.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoPuzzle : MonoBehaviour
+{
+    [SerializeField]
+    Inventory plyInv;
+    [SerializeField]
+    LPhotoCntrl[] photos;
+    [SerializeField]
+    GameObject puzzleGO;
+    [SerializeField]
+    PicSlot[] Slots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (puzzleGO.activeSelf)
+        {
+            for (int i = 0; i < photos.Length; i++)
+            {
+                if (plyInv.ContainsItem(photos[i].myPhoto))
+                {
+                    if (photos[i].me != null)
+                    {
+                        photos[i].me.enabled = true;
+                    }
+                }
+                else
+                {
+                    if (photos[i].me != null)
+                    {
+                        photos[i].me.enabled = false;
+                    }
+                }
+            }
+        }
+    }
+}
