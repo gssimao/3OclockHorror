595219b34f54c219fbe4668c7cb818225092ce23
Noah <noah.osborn@trojans.dsu.edu>
2021-02-04 18:09:32 -0600

    v3.028.7
    Began implementing dynamic tasks

--- 3OclockHorror/Assets/Scripts/TaskListTracker.cs
+++ 3OclockHorror/Assets/Scripts/TaskListTracker.cs
@@ -1,13 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TaskListTracker : MonoBehaviour
 {
+    //Logic flow:
+    /**
+     Explore the house
+        - Photo
+            - Coin
+                - Bust
+
+        - Daddy's note
+    **/
+    //Needed:
+    /*
+     * Task list reference
+     * bools to track the stage the player is at
+     * reference to popup notif
+     */
+    [SerializeField]
+    Text taskList;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Set base task to explore house
+        taskList.text = "- Explore the house";
     }
 
     // Update is called once per frame
