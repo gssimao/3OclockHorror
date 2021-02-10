8fe2a91d206f606f51ec2eb60842daad67daf6ad
Noah <noah.osborn@trojans.dsu.edu>
2021-02-10 16:27:03 -0600

    v4.03.01
    Moved more files to their rightful place. Scene tester is now empty and ready for deletion. Created a new scene for testing ideas for dropdown task gui. Implemented esc menu in the start scene as well, without ability to exit to main menu.

---
+++ 3OclockHorror/Assets/Scripts/CursorController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+
+    public Texture2D cursorArrow;
+    public Texture2D cursorDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.visible = false;
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.Auto);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //transform.position = cursorPos;
+    }
+
+    private void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorDown, Vector2.zero, CursorMode.Auto);
+    }
+
+    private void OnMouseExit()
+    {
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.Auto);
+    }
+}
