2eb1ed85990b0ef7fb6f643ab1267b424f741f2b
Noah <noah.osborn@trojans.dsu.edu>
2021-02-16 17:26:59 -0600

    v4.09.1
    The riddles for the skull puzzle have been implemented.

---
+++ 3OclockHorror/Assets/riddleOpener.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class riddleOpener : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    GameObject riddleCanv;
+    [SerializeField]
+    GameObject riddle;
+    [SerializeField]
+    invInput Listener;
+    [SerializeField]
+    KeyCode interactKey;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector2.Distance(player.transform.position, this.transform.position);
+
+        if (dist <= 0.45f)
+        {
+            Listener.enabled = false;
+            if (Input.GetKeyDown(interactKey))
+            {
+                riddleCanv.SetActive(true);
+                riddle.SetActive(true);
+            }
+        }
+    }
+}
