7677d33b89124c00eb3c71eb2595e45c8559e7f2
Ryan <ryan.marnette@yahoo.com>
2021-02-24 16:41:42 -0600

    v5.05.2
    Finished placing notes on the second floor. Cleaned up the ObjectRender code.

--- 3OclockHorror/Assets/Scripts/ObjectRender.cs
+++ 3OclockHorror/Assets/Scripts/ObjectRender.cs
@@ -8,30 +8,29 @@ public class ObjectRender : MonoBehaviour
     private GameObject item;
     public Sprite sprite;
     private Image image;
+    private PlayerMovement Player;
     [TextArea]
     public string Note;
     private GameObject NoteText;
     private Sprite transparent;
     public bool active;
     public invInput Listener;
-    public workbench_cntrl workbench;
     public bool colliding;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         colliding = true;
-        Debug.Log("Entered collider");
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        Listener.enabled = false;
+        //Listener.enabled = false;
         
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        Listener.enabled = true;
+        //Listener.enabled = true;
         colliding = false;
         image.sprite = transparent;
     }
@@ -40,22 +39,21 @@ public class ObjectRender : MonoBehaviour
     {
         if (colliding)
         {
-            //Input.GetKeyDown(KeyCode.F) &&  
             if (Input.GetKeyDown(KeyCode.F) && !active)
             {
-                Debug.Log("key pressed");
                 active = true;
+                Player.enabled = false;
                 image.sprite = sprite;
                 NoteText.SetActive(true);
                 NoteText.GetComponent<Text>().text = Note;
                 
             }
-            //Input.GetKeyDown(KeyCode.F) && 
             else if (Input.GetKeyDown(KeyCode.F) && active)
             {
                 NoteText.GetComponent<Text>().text = "";
                 NoteText.SetActive(false);
                 image.sprite = transparent;
+                Player.enabled = true;
                 active = false;
             }
         }
@@ -65,6 +63,7 @@ public class ObjectRender : MonoBehaviour
     {
         active = false;
         colliding = false;
+        Player = GameObject.Find("Player2").GetComponent<PlayerMovement>();
         item = GameObject.Find("Image");
         image = GameObject.Find("PopupImage").GetComponent<Image>();
         NoteText = GameObject.Find("NoteText");
