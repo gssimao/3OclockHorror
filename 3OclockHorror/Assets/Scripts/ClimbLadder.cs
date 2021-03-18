c3457c2d9aaddecfd8ac7ec388d7b1f81ba343aa
Benjamin <benjamin.henschen@gmail.com>
2021-03-18 17:36:53 -0500

    v5.26.4
    You can now move between the right side rooms

--- 3OclockHorror/Assets/Scripts/ClimbLadder.cs
+++ 3OclockHorror/Assets/Scripts/ClimbLadder.cs
@@ -8,6 +8,10 @@ public class ClimbLadder : MonoBehaviour
     GameObject player;
     [SerializeField]
     GameObject dest;
+    [SerializeField]
+    room destRoom;
+    [SerializeField]
+    invInput Listener;
 
     float dist;
     // Start is called before the first frame update
@@ -22,10 +26,17 @@ public class ClimbLadder : MonoBehaviour
         dist = Vector3.Distance(this.transform.position, player.transform.position);
         if(dist <= 0.5f)
         {
+            Listener.isFocus = false;
             if(Input.GetKeyDown("e"))
             {
-                player.transform.position = dest.transform.position;
+                UpdatePlayer();
             }
         }
+    }
+
+    void UpdatePlayer()
+    {
+        player.transform.position = dest.transform.position;
+        player.GetComponent<PlayerMovement>().changeRoom(destRoom);
     }
 }
