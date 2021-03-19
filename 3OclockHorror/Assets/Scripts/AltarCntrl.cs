7c4b1b08d40ac7d8e30c0605981b081a5c8dced1
Noah <noah.osborn@trojans.dsu.edu>
2021-03-19 15:58:32 -0500

    v5.27.1
    Working on Book puzzle and preparing for ending assets

---
+++ 3OclockHorror/Assets/Scripts/AltarCntrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AltarCntrl : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    GameObject rightEnding;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(this.gameObject.transform.position, player.transform.position);
+        if (dist < 1.0f)
+        {
+            rightEnding.SetActive(true);
+        }
+    }
+}
