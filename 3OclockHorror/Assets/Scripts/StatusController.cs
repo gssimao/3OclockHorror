fba808d877c6947a95ffc565eb22e4dced4c031c
Benjamin <benjamin.henschen@gmail.com>
2021-03-22 08:22:06 -0500

    v5.28.1
    Added new Sanity Text

--- 3OclockHorror/Assets/Scripts/StatusController.cs
+++ 3OclockHorror/Assets/Scripts/StatusController.cs
@@ -26,23 +26,27 @@ public class StatusController : MonoBehaviour
         {
             if(sanity.sanityValue > 80)
             {
-                statusText.text = "Feeling healthy, I have heard what this house does to the minds of people. Keeping this journal will help me preserve my sanity.";
+                statusText.text = "High Sanity \n Feeling healthy, I have heard what this house does to the minds of people. Keeping this journal will help me preserve my sanity.";
             }
             else if(sanity.sanityValue > 60)
             {
-                statusText.text = "I am starting to feel dizzy, not too bad though. I wonder how much worse it will get.";
+                statusText.text = "Mid High Sanity \n I am starting to feel dizzy, not too bad though. I wonder how much worse it will get.";
             }
             else if(sanity.sanityValue > 40)
             {
-                statusText.text = "Starting to feel nauseous the house is takng a toll on my mind.";
+                statusText.text = "Mid Low Sanity \n Starting to feel nauseous the house is takng a toll on my mind.";
             }
             else if(sanity.sanityValue > 20)
             {
-                statusText.text = "Seeng things, rooms warping hed throbbing. Cant last much longer.";
+                statusText.text = "Low Sanity \n Seeng things, rooms warping hed throbbing. Cant last much longer.";
+            }
+            else if(sanity.sanityValue > 0)
+            {
+                statusText.text = "Almost Insane \n thbreating inheavg mind linveag rnnuing yawa pasceing";
             }
             else
             {
-                statusText.text = "thbreating inheavg mind linveag rnnuing yawa pasceing";
+                statusText.text = "Insane";
             }
         }
     }
