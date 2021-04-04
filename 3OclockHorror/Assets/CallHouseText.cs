9284951d739f47e145ffba1e864920634b376107
gabe <gabriel.simao@trojans.dsu.edu>
2021-04-04 01:20:44 -0500

    v6.12

---
+++ 3OclockHorror/Assets/CallHouseText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CallHouseText : MonoBehaviour
+{
+    [SerializeField] public Writer textWriter;
+    public Text dialog;
+    string message = "teeeesstt 12345601 29309 12031209302 1930910219 309102 193091021 9309102 193 0910219 3091021 930910 219309129";
+    //this is where the maneger will decide what to play and when to play it.
+
+    private void Start() // this is for testing
+    {
+        textWriter.AddWriter(dialog, message, .1f, true);
+    }
+    public void WriteString(string message) // this is where the calls will happen
+    {
+        textWriter.AddWriter(dialog, message, .1f, true);
+    }
+}
---
+++ 3OclockHorror/Assets/Writer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Writer : MonoBehaviour
+{
+    public Text uiText;
+    public string textToWrite;
+    public int characterIndex;
+    public float timePerCharacter;
+    public float timer;
+    private bool invisibleCharacters;
+   public void AddWriter(Text uiText, string textToWrite, float timePerCharacter, bool invisibleCharacters)
+    {
+        this.uiText = uiText;
+        this.textToWrite = textToWrite;
+        this.timePerCharacter = timePerCharacter;
+        this.invisibleCharacters = invisibleCharacters;
+        characterIndex = 0;
+    }
+    void Update()
+    {
+        if(uiText != null)
+        {
+            timer -= Time.deltaTime;
+            while(timer <= 0f)
+            {
+                //display next character
+                timer += timePerCharacter;
+                characterIndex++;
+                string text = textToWrite.Substring(0, characterIndex);
+                if(invisibleCharacters)
+                {
+                    text += "<color=#00000000>" + textToWrite.Substring(characterIndex) + "</color>";
+                }
+                uiText.text = text;
+                if(characterIndex >= textToWrite.Length)
+                {
+                    uiText = null;
+                    return;
+                }
+            }
+        }
+    }
+}
