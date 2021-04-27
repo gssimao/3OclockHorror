94f00ce94f6fc323ae63428160c6fa1f7614342b
Noah <noah.osborn@trojans.dsu.edu>
2021-04-27 17:43:09 -0500

    v6.64
    Options added back in. Volume works but windowed mode does not yet

---
+++ 3OclockHorror/Assets/volumeHolder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class volumeHolder : MonoBehaviour
+{
+    public static volumeHolder instance;
+    [SerializeField]
+    float volume;
+    AudioManager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null && instance != null)
+        {
+            Destroy(gameObject); //Is there a manager? If yes then I'm gone
+        }
+        else
+        {
+            instance = this;  //There isnt a manager? I'm it
+            DontDestroyOnLoad(gameObject);
+        }
+
+        setVolume();
+
+    }
+
+    public void OnSliderValueChanged(float value)
+    {
+        volume = value;
+        setVolume();
+    }
+
+    public void setVolume()
+    {
+        manager = FindObjectOfType<AudioManager>();
+        manager.updateVolume(volume);
+    }
+}
