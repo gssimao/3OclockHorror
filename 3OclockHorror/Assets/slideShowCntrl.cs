7a60c207d10368fc606ada90461438ac56395c7c
Noah <noah.osborn@trojans.dsu.edu>
2021-05-01 13:51:18 -0500

    v6.50.13
    Added in the slide show for the end

---
+++ 3OclockHorror/Assets/slideShowCntrl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slideShowCntrl : MonoBehaviour
+{
+    [SerializeField]
+    GameObject imageone;
+    [SerializeField]
+    GameObject imagetwo;
+
+    float time = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if(time > 2)
+        {
+            imageone.SetActive(false);
+            imagetwo.SetActive(true);
+        }
+        if(time > 4)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
